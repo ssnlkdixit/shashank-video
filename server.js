f12de24c282e61424d8b55a79f018b8a3e738510
ssnlkdixit <ssnkdixit@gmail.com>
2020-04-29 22:33:01 +0530

    first commi

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+var port = process.env.PORT || 3004;
+
+
+// Set public folder as root
+app.use(express.static('public'));
+
+// Provide access to node_modules folder from the client-side
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+//shashank
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+  console.info('listening on %d', port);
+});
